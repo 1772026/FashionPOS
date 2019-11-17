a5ba0957391a1a16cc34aacdbba2d80513ef04a5
1772026 <it1772026@student.it.maranatha.edu>
2019-11-18 03:44:22 +0700

    Revert "finish progress backlog login page"
    This reverts commit 4730982fa46cbad7633215efcf244f4a9f7f5541.

--- DesktopFashionPOS/src/fashionPOS/Controller/HomeController.java
+++
@@ -1,9 +0,0 @@
-package fashionPOS.Controller;
-
-public class HomeController {
-    private LoginController loginController;
-
-    public void setMainFormController(LoginController mainFormController) {
-        this.loginController=loginController;
-    }
-}
---
+++ DesktopFashionPOS/src/fashionPOS/Controller/loginController.java
@@ -0,0 +1,17 @@
+package fashionPOS.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class loginController {
+    @FXML
+    private TextField txtUsername;
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private void actionLogin(ActionEvent actionEvent) {
+    }
+}
