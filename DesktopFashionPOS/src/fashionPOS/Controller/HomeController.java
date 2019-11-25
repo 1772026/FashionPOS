884816bbb5d844e2f83e9d5011886dc13323c08d
1772026 <it1772026@student.it.maranatha.edu>
2019-11-25 16:40:44 +0700

    update project

--- DesktopFashionPOS/src/fashionPOS/Controller/HomeController.java
+++ DesktopFashionPOS/src/fashionPOS/Controller/HomeController.java
@@ -1,9 +1,49 @@
 package fashionPOS.Controller;
 
-public class HomeController {
-    private LoginController loginController;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
-    public void setMainFormController(LoginController mainFormController) {
-        this.loginController=loginController;
+import java.io.IOException;
+
+public class HomeController {
+    private Stage userStage;
+    private Stage itemStage;
+    public void actionFormItem(ActionEvent actionEvent) {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/fashionPOS/View/item.fxml"));
+            AnchorPane pane = loader.load();
+//                HomeController controller = loader.getController();
+//                controller.setMainFormController(this);
+//            Scene scene = new Scene(pane);
+            Scene scene = new Scene(pane);
+            userStage.setScene(scene);
+            userStage.setTitle("Item");
+            userStage.initModality(Modality.APPLICATION_MODAL);
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+    }
+
+    public void actionFormUser(ActionEvent actionEvent) {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/fashionPOS/View/user.fxml"));
+            AnchorPane pane = loader.load();
+//                HomeController controller = loader.getController();
+//                controller.setMainFormController(this);
+            Scene scene = new Scene(pane);
+            itemStage.setScene(scene);
+            itemStage.setTitle("User");
+            itemStage.initModality(Modality.APPLICATION_MODAL);
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
     }
 }
--- DesktopFashionPOS/src/fashionPOS/Controller/LoginController.java
+++ DesktopFashionPOS/src/fashionPOS/Controller/LoginController.java
@@ -2,7 +2,6 @@ package fashionPOS.Controller;
 
 import fashionPOS.Model.Dao.LoginDao;
 import fashionPOS.Model.Entity.Tbuser;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -18,7 +17,9 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class LoginController {
+    public AnchorPane root;
     private Stage homeStage;
+    private Stage self;
     private LoginDao loginDao;
     @FXML
     private PasswordField txtUsername;
@@ -44,8 +45,8 @@ public class LoginController {
         if (getLogin(tbuser)) {
             try {
                 FXMLLoader loader = new FXMLLoader();
-                loader.setLocation(getClass().getResource("/fashionPOS/View/item.fxml"));
-                AnchorPane pane = loader.load();
+                loader.setLocation(getClass().getResource("/fashionPOS/View/home.fxml"));
+                BorderPane pane = loader.load();
 //                HomeController controller = loader.getController();
 //                controller.setMainFormController(this);
                 Scene scene = new Scene(pane);
@@ -53,6 +54,8 @@ public class LoginController {
                 homeStage.setScene(scene);
                 homeStage.setTitle("Home");
                 homeStage.initModality(Modality.APPLICATION_MODAL);
+
+                ((Stage)root.getScene().getWindow() ).close();
             } catch (IOException e) {
                 e.printStackTrace();
 
