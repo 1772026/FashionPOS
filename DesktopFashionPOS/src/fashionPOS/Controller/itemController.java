4a2cc2147315f7bac973e0f507ab2987d76eaa0b
Nicolavickh <nicolavickh@gmail.com>
2020-01-09 00:21:24 +0700

    itemcontroller dan view

--- DesktopFashionPOS/src/fashionPOS/Controller/itemController.java
+++ DesktopFashionPOS/src/fashionPOS/Controller/itemController.java
@@ -1,12 +1,24 @@
 package fashionPOS.Controller;
 
+import fashionPOS.Model.Dao.CategoryDao;
+import fashionPOS.Model.Dao.ItemDao;
+import fashionPOS.Model.Entity.Tbcategory;
+import fashionPOS.Model.Entity.Tbitem;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 
-public class itemController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class itemController implements Initializable {
     @FXML
     private TextField txtid;
     @FXML
@@ -16,7 +28,7 @@ public class itemController {
     @FXML
     private TextField txtPriceSell;
     @FXML
-    private ComboBox comboCategory;
+    private ComboBox<Tbcategory> comboCategory;
     @FXML
     private Button save;
     @FXML
@@ -30,19 +42,41 @@ public class itemController {
     @FXML
     private TextArea txtDescription;
     @FXML
-    private TableView tableItem;
+    private TableView<Tbitem> tableItem;
     @FXML
-    private TableColumn colID;
+    private TableColumn<Tbitem, String> colID;
     @FXML
-    private TableColumn colName;
+    private TableColumn<Tbitem, String> colName;
     @FXML
-    private TableColumn colCategory;
+    private TableColumn<Tbitem, String> colCategory;
     @FXML
-    private TableColumn colPriceSell;
+    private TableColumn<Tbitem, String> colPriceSell;
     @FXML
-    private TableColumn colPriceBuy;
+    private TableColumn<Tbitem, String> colPriceBuy;
     @FXML
     private VBox child;
+    private Tbitem selected;
+    private ObservableList<Tbitem> tbItems;
+    private ObservableList<Tbcategory> tbCategories;
+    private ItemDao itemDao = new ItemDao();
+    private CategoryDao categoryDao = new CategoryDao();
+
+
+    private ObservableList<Tbitem> getTbItems() {
+        if (tbItems == null) {
+            tbItems = FXCollections.observableArrayList();
+            tbItems.addAll(itemDao.getAllData());
+        }
+        return tbItems;
+    }
+
+    public ObservableList<Tbcategory> getCategories() {
+        if (tbCategories == null) {
+            tbCategories = FXCollections.observableArrayList();
+            tbCategories.addAll(categoryDao.getAllData());
+        }
+        return tbCategories;
+    }
 
     @FXML
     private void categoryClick(MouseEvent mouseEvent) {
@@ -50,21 +84,103 @@ public class itemController {
 
     @FXML
     private void save(ActionEvent actionEvent) {
+        Tbitem item = new Tbitem();
+        Tbcategory category = new Tbcategory();
+        category.setCategoryId(comboCategory.getSelectionModel().getSelectedItem().getCategoryId());
+
+        item.setItemName(txtName.getText().trim());
+        item.setItemPriceSell(Integer.parseInt(txtPriceSell.getText().trim()));
+        item.setItemPriceSupply(Integer.parseInt(txtPriceBuy.getText().trim()));
+        item.setItemDescription(txtDescription.getText());
+        item.setTbcategoryByTbcategoryCategoryId(category);
+        boolean notDuplicate = getTbItems().stream().noneMatch(d -> d.getItemName() == item.getItemName());
+
+        if (notDuplicate) {
+            itemDao.addData(item);
+            clearForm();
+        } else {
+            Alert a = new Alert(Alert.AlertType.ERROR);
+            a.setContentText("Duplicate Item Name");
+            a.showAndWait();
+        }
     }
 
     @FXML
     private void reset(ActionEvent actionEvent) {
+        clearForm();
     }
 
     @FXML
     private void update(ActionEvent actionEvent) {
+        Tbitem item = new Tbitem();
+        Tbcategory category = new Tbcategory();
+        category.setCategoryId(comboCategory.getSelectionModel().getSelectedItem().getCategoryId());
+
+        item.setItemId(Integer.parseInt(txtid.getText()));
+        item.setItemName(txtName.getText().trim());
+        item.setItemPriceSell(Integer.parseInt(txtPriceSell.getText().trim()));
+        item.setItemPriceSupply(Integer.parseInt(txtPriceBuy.getText().trim()));
+        item.setItemDescription(txtDescription.getText());
+        item.setTbcategoryByTbcategoryCategoryId(category);
+        boolean notDuplicate = getTbItems().stream().noneMatch(d -> d.getItemName() == item.getItemName());
+        if (notDuplicate) {
+            itemDao.updateData(item);
+            clearForm();
+        } else {
+            Alert a = new Alert(Alert.AlertType.ERROR);
+            a.setContentText("Duplicate Item Name");
+            a.showAndWait();
+        }
     }
 
     @FXML
     private void delete(ActionEvent actionEvent) {
+        Tbitem item = new Tbitem();
+        item.setItemId(Integer.parseInt(txtid.getText()));
+        itemDao.updateData(item);
+        clearForm();
     }
 
     @FXML
-    private void tableCliked(MouseEvent mouseEvent) {
+    private void tableClicked(MouseEvent mouseEvent) {
+        selected = tableItem.getSelectionModel().getSelectedItem();
+        txtid.setText(String.valueOf(selected.getItemId()));
+        txtName.setText(selected.getItemName());
+        txtPriceSell.setText(String.valueOf(selected.getItemPriceSell()));
+        txtPriceBuy.setText(String.valueOf(selected.getItemPriceSupply()));
+        txtDescription.setText(selected.getItemDescription());
+        save.setDisable(true);
+        update.setDisable(false);
+        delete.setDisable(false);
     }
+
+    private void clearForm() {
+        txtid.clear();
+        txtDescription.clear();
+        txtName.clear();
+        txtPriceBuy.clear();
+        txtPriceSell.clear();
+        comboCategory.setValue(null);
+        save.setDisable(false);
+        update.setDisable(true);
+        delete.setDisable(true);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        tableItem.setItems(getTbItems());
+//        comboCategory.setItems(getCategories());
+        colID.setCellValueFactory(data ->
+                new SimpleStringProperty(String.valueOf(data.getValue().getItemId())));
+        colName.setCellValueFactory(data ->
+                new SimpleStringProperty(data.getValue().getItemName()));
+        colCategory.setCellValueFactory(data ->
+                new SimpleStringProperty(String.valueOf(data.getValue().getTbcategoryByTbcategoryCategoryId().getCategoryId())));
+        colPriceBuy.setCellValueFactory(data ->
+                new SimpleStringProperty(String.valueOf(data.getValue().getItemPriceSupply())));
+        colPriceSell.setCellValueFactory(data ->
+                new SimpleStringProperty(String.valueOf(data.getValue().getItemPriceSell())));
+    }
+
+
 }
--- DesktopFashionPOS/src/fashionPOS/Main.java
+++ DesktopFashionPOS/src/fashionPOS/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
 //        Parent root = FXMLLoader.load(getClass().getResource("View/login.fxml"));
-        Parent root = FXMLLoader.load(getClass().getResource("View/login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("View/ItemForm.fxml"));
         primaryStage.setTitle("Fashion POS");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
