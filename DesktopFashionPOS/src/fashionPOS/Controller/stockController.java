32729ca1020e224057d4fe48516f3844e627e5d4
Nicolavickh <nicolavickh@gmail.com>
2020-01-09 12:19:45 +0700

    stock Controller and View

--- DesktopFashionPOS/src/fashionPOS/Controller/stockController.java
+++ DesktopFashionPOS/src/fashionPOS/Controller/stockController.java
@@ -1,14 +1,25 @@
 package fashionPOS.Controller;
 
+import fashionPOS.Model.Dao.SizeStockDao;
+import fashionPOS.Model.Entity.Tbitem;
+import fashionPOS.Model.Entity.Tbsizestock;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 
-public class stockController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class stockController implements Initializable {
     @FXML
     private TextField txtItem;
     @FXML
@@ -32,31 +43,81 @@ public class stockController {
     @FXML
     private Button delete;
     @FXML
-    private TableView tableStock;
+    private TableView<Tbsizestock> tableStock;
     @FXML
-    private TableColumn colName;
+    private TableColumn<Tbsizestock, String> colName;
     @FXML
-    private TableColumn colS;
+    private TableColumn<Tbsizestock, String> colS;
     @FXML
-    private TableColumn colL;
+    private TableColumn<Tbsizestock, String> colL;
     @FXML
-    private TableColumn colM;
+    private TableColumn<Tbsizestock, String> colM;
     @FXML
-    private TableColumn colXL;
+    private TableColumn<Tbsizestock, String> colXL;
     @FXML
-    private TableColumn colXXL;
+    private TableColumn<Tbsizestock, String> colXXL;
     @FXML
-    private TableColumn colXXXL;
+    private TableColumn<Tbsizestock, String> colXXXL;
+    private Tbsizestock selected = new Tbsizestock();
+    private SizeStockDao stockDao = new SizeStockDao();
+    private ObservableList<Tbsizestock> stocks;
 
+    private ObservableList<Tbsizestock> getStock() {
+        if (stocks == null) {
+            stocks = FXCollections.observableArrayList();
+            stocks.addAll(stockDao.getAllData());
+        }
+        return stocks;
+    }
     @FXML
     private void add(ActionEvent actionEvent) {
+        Tbsizestock stock = new Tbsizestock();
+        stock.setTbitemByTbitemItemId(selected.getTbitemByTbitemItemId());
+        stock.setSizestockSStock(selected.getSizestockSStock() + Integer.parseInt(txtS.getText()));
+        stock.setSizestockMStock(selected.getSizestockMStock() + Integer.parseInt(txtM.getText()));
+        stock.setSizestockLStock(selected.getSizestockLStock() + Integer.parseInt(txtL.getText()));
+        stock.setSizestockXlStock(selected.getSizestockXlStock() + Integer.parseInt(txtXL.getText()));
+        stock.setSizestockXxlStock(selected.getSizestockXxlStock() + Integer.parseInt(txtXXL.getText()));
+        stock.setSizestockXxxlStock(selected.getSizestockXxxlStock() + Integer.parseInt(txtXXXL.getText()));
+        stockDao.updateData(stock);
     }
 
     @FXML
     private void update(ActionEvent actionEvent) {
+        Tbsizestock stock = new Tbsizestock();
+        stock.setTbitemByTbitemItemId(selected.getTbitemByTbitemItemId());
+        stock.setSizestockSStock(Integer.parseInt(txtS.getText()));
+        stock.setSizestockMStock(Integer.parseInt(txtM.getText()));
+        stock.setSizestockLStock(Integer.parseInt(txtL.getText()));
+        stock.setSizestockXlStock(Integer.parseInt(txtXL.getText()));
+        stock.setSizestockXxlStock(Integer.parseInt(txtXXL.getText()));
+        stock.setSizestockXxxlStock(Integer.parseInt(txtXXXL.getText()));
+        stockDao.updateData(stock);
     }
 
+
     @FXML
-    private void delete(ActionEvent actionEvent) {
+    private void tableClicked(MouseEvent mouseEvent) {
+        selected = tableStock.getSelectionModel().getSelectedItem();
+        txtItem.setText(selected.getTbitemByTbitemItemId().getItemName());
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        tableStock.setItems(getStock());
+        colName.setCellValueFactory(data ->
+                new SimpleStringProperty(String.valueOf(data.getValue().getTbitemByTbitemItemId().getItemName())));
+        colS.setCellValueFactory(data ->
+                new SimpleStringProperty(String.valueOf(data.getValue().getSizestockSStock())));
+        colM.setCellValueFactory(data ->
+                new SimpleStringProperty(String.valueOf(data.getValue().getSizestockMStock())));
+        colL.setCellValueFactory(data ->
+                new SimpleStringProperty(String.valueOf(data.getValue().getSizestockLStock())));
+        colXL.setCellValueFactory(data ->
+                new SimpleStringProperty(String.valueOf(data.getValue().getSizestockXlStock())));
+        colXXL.setCellValueFactory(data ->
+                new SimpleStringProperty(String.valueOf(data.getValue().getSizestockXxlStock())));
+        colXXXL.setCellValueFactory(data ->
+                new SimpleStringProperty(String.valueOf(data.getValue().getSizestockXxxlStock())));
     }
 }
